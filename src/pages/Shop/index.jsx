7f297b44e5730762e99e8d53d122efc33e12b27b
Lucas Gomes <lucasgomesbt@gmail.com>
2021-12-13 23:39:25 -0300

    FIX: Adiçao da correçao do problema com os endereços das imagens, colocando o require dentro do boots.js e com acessando o endereço via propriedade default

--- src/pages/Shop/index.jsx
+++ src/pages/Shop/index.jsx
@@ -1,17 +1,13 @@
 import Item from "../../components/Item";
-// import tenis1Img from "../../assets/tenis1.png";
-
 import styles from "./styles.module.scss";
 import { listAllBoots } from "../../utils/boots";
 
 export default function Shop() {
-    console.log(listAllBoots);
-
     return (
         <>
             <main className={styles.items}>
                 {listAllBoots.map((book) => (
-                    <Item key={book.id} title={book.title} srcImg="https://via.placeholder.com/400" imgAlt={book.alt} priceDanger={book.priceBefore} priceCurrent={book.priceCurrent} />
+                    <Item key={book.id} title={book.title} srcImg={book.endImg} imgAlt={book.alt} priceDanger={book.priceBefore} priceCurrent={book.priceCurrent} />
                 ))};
             </main>
         </>
